6b5be5b1429341dd322eea212600e35c3b0f3b9a
Bantuan adakami <144202158+cs-adakami@users.noreply.github.com>
2026-01-19 22:06:27 +0700

    Update contact information in App component

--- src/App.tsx
+++ src/App.tsx
@@ -14,8 +14,8 @@ import {
 
 function App() {
   const contactInfo = {
-    wa: '628137750397',
-    phone: '08137750397',
+    wa: '628216364259',
+    phone: '08216364259',
     address: 'Gedung Cyber 2 Tower Lantai 28 Jl. HR Rasuna Said Blok X-5 No.13 RT/RW. 007/002 Kuningan Timur, Setia Budi, Jakarta Selatan 12950'
   };
   
